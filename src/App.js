6162109c42ef0b29fd73814ea64acc40c2114f83
Anthony EZEAMAKA <anthony.ezeamaka@external.totalenergies.com>
2022-04-13 17:08:52 +0100

    Add project files.

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Sidebar from 'components/Sidebar';
+import Dashboard from 'pages/Dashboard';
+import Settings from 'pages/Settings';
+import Tables from 'pages/Tables';
+import Maps from 'pages/Maps';
+import Footer from 'components/Footer';
+import Landing from 'pages/Landing';
+import Profile from 'pages/Profile';
+import Login from 'pages/Login';
+import Register from 'pages/Register';
+
+// Tailwind CSS Style Sheet
+import 'assets/styles/tailwind.css';
+
+function App() {
+    return (
+        <>
+            <Sidebar />
+            <div className="md:ml-64">
+                <Switch>
+                    <Route exact path="/" component={Dashboard} />
+                    <Route exact path="/settings" component={Settings} />
+                    <Route exact path="/tables" component={Tables} />
+                    <Route exact path="/maps" component={Maps} />
+                    <Route exact path="/profile" component={Profile} />
+                    <Route exact path="/login" component={Login} />
+                    <Route exact path="/register" component={Register} />
+                    <Redirect from="*" to="/" />
+                </Switch>
+                <Footer />
+            </div>
+        </>
+    );
+}
+
+export default App;
